e53aadb3167430ef527a5e6741e888dda7773079
Sobol Eduard <sobol.edik@gmail.com>
2025-01-14 20:54:17 +0100

    init

---
+++ android/app/src/main/kotlin/com/example/screenify/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.screenify
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  const String s = String.fromEnvironment("TEST");
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(s),
+        ),
+      ),
+    ),
+  );
+}
